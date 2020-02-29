898e1bc69d7b09cd6617af83aed96e30a42c3505
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-02-29 09:47:20 -0800

    mvp met
    iterative completed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,21 +1,34 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
-# loop through n-1 elements
-# repeat (numOfElements - 1) times
+#     for i in range(0, len(arr) - 1):
+# #   set the first unsorted element as the minimum
+#         cur_index = i
+#         smallest_index = cur_index
+#         # TO-DO: find next smallest element
+# #   for each of the unsorted elements
+#         # LOOP thru remaining array to right!
+#         for cur_index in range(i + 1, len(arr)):
+#             if arr[cur_index] < arr[smallest_index]:
+#                 # set element as new minimum
+#                 smallest_index = cur_index 
+#                 # swap
+#                 arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
+#             ++i
+    temp = 0
+
     for i in range(0, len(arr) - 1):
-#   set the first unsorted element as the minimum
         cur_index = i
+
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-#   for each of the unsorted elements
-        # LOOP thru remaining array to right!
-        for cur_index in range(i + 1, len(arr)):
-            if arr[cur_index] < arr[smallest_index]:
-                # set element as new minimum
-                cur_index = smallest_index 
-                # swap
-                arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
-            ++i
+
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+
+        if cur_index != smallest_index:
+            temp = arr[cur_index]
+            arr[cur_index] = arr[smallest_index]
+            arr[smallest_index] = temp
     return arr
 
 #     repeat (numOfElements - 1) times
@@ -33,7 +46,7 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):    
-
+    
     #double loops so that the program can go through each iteration of the array n(the length of the array) times
     #if there are 4 items in an array, the program will iterate through the array a total of 16 times
     #outer loop will execute the inner loop 4 times
@@ -51,31 +64,3 @@ def bubble_sort( arr ):
 def count_sort( arr, maximum=-1 ):
 
     return arr
-
-# def selection_sort( arr ):
-#     temp = 0
-#     # loop through n-1 elements
-#     for i in range(0, len(arr) - 1):
-#         cur_index = i
-
-#         #1st time through => store the first value as the smallest element we have seen so far
-#         #i or current index will always be the smallest index on subsequent iterations and used for comparisons 
-#         #because the indexes before were already determined to be smaller than the current index
-#         smallest_index = cur_index 
-#         # TO-DO: find next smallest element
-#         # (hint, can do in 3 loc) 
-#         #if item at arr[j] which the 1st time through will be arr[1] is < arr[smallest_index]
-#         #arr[smallest_index] the 1st time though is arr[0]
-#         for j in range(i + 1, len(arr)):
-#             if arr[j] < arr[smallest_index]:               
-#                 smallest_index = j #1st time through=> now the smallest index will be pointing to 1      
-            
-#         # TO-DO: swap  
-#         #if arr[i] (i is cur_inde) != smallest_index (j)
-#         #
-#         if cur_index != smallest_index:   
-#             temp = arr[cur_index]
-#             arr[cur_index] = arr[smallest_index]
-#             arr[smallest_index] = temp
-
-#     return arr
