a393f29a5d768fe0ef4f00088f6f673efffd7bde
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-03-02 15:02:19 -0800

    merge done
    bugged

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,14 +1,41 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
+# TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
+    # starting at beginning of `a` and `b`
+    # compare the next value of each
+    # add smallest to `merged_arr`
+
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
+   
     # TO-DO
-    
+    i = 0
+    j = 0
+    # perform the actions thru both arrays
+    while i < len(arrA) and j < len(arrB):
+        if arrB[j] > arrA[i]:
+            merged_arr.append(arrA[i])
+            ++i
+        else:
+            merged_arr.append(arrB[j])
+            ++j
+
+    while i < len(arrA):
+        merged_arr.append(arrA[i])
+        ++i
+
+    while j < len(arrB):
+        merged_arr.append(arrB[j])
+        ++j
+        
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
+    #  if len( arr ) > 1: 
+        # recursively call merge_sort() on LHS
+        # recursively call merge_sort() on RHS
+        # merge sorted pieces
     # TO-DO
 
     return arr
@@ -31,3 +58,19 @@ def merge_sort_in_place(arr, l, r):
 def timsort( arr ):
 
     return arr
+
+def merge_sort( arr ):
+    if len( arr ) > 1: 
+        # recursively call merge_sort() on LHS
+        # recursively call merge_sort() on RHS
+        # merge sorted pieces
+
+def merge_helper( a, b ):
+    merged_arr = []
+
+    # starting at beginning of `a` and `b`
+    # compare the next value of each
+    # add smallest to `merged_arr`
+
+    return merged_arr
+
